204ac371130e1728625670c92c6ffd5ccbeda35a
Amelia McNamara <amelia.a.mcnamara@gmail.com>
2026-08-10 13:43:28 -0500

    added documentation for AR1

--- R/AR1.R
+++ R/AR1.R
@@ -1,3 +1,14 @@
+#' First-order autoregressive model
+#'
+#' This function is a wrapper around the forecast::Arima() function.
+#' It creates a first-order autoregressive model with a constant term.
+#'
+#' @param x a vector, ideally a timeseries vector
+#'
+#' @returns an Arima model
+#' @export
+#'
+#' @examples
 AR1 <- function(x){
   forecast::Arima(x, order = c(1, 0, 0), include.constant = TRUE)
 }
