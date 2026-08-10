5df4910c4d03be31e272248b99c10e2080d5ba15
Amelia McNamara <amelia.a.mcnamara@gmail.com>
2026-08-10 14:30:44 -0500

    document data

---
+++ R/data.R
@@ -0,0 +1,53 @@
+#' FRED Bacon data
+#'
+#' @description
+#' A subset of the time series APU0000712311 from FRED, data on bacon
+#' prices from 2016 to 2026
+#'
+#'
+#' @format ## `bacon`
+#' A data frame with 119 rows and 5 columns:
+#' \describe{
+#'   \item{date}{a Date object with the data of the data}
+#'   \item{series_id}{APU0000712311, bacon series}
+#'   \item{value}{the average price of bacon}
+#'   \item{realtime_start, realtime_end}{The date the data was downloaded}
+#' }
+#' @source <https://fred.stlouisfed.org/series/APU0000712311>
+"bacon"
+
+#' FRED Lettuce data
+#'
+#' @description
+#' A subset of the time series APU0000712211 from FRED, data on lettuce
+#' prices from 2016 to 2026
+#'
+#'
+#' @format ## `lettuce`
+#' A data frame with 119 rows and 5 columns:
+#' \describe{
+#'   \item{date}{a Date object with the data of the data}
+#'   \item{series_id}{APU0000712311, lettuce series}
+#'   \item{value}{the average price of iceberg lettuce}
+#'   \item{realtime_start, realtime_end}{The date the data was downloaded}
+#' }
+#' @source <https://fred.stlouisfed.org/series/APU0000712211>
+"lettuce"
+
+#' FRED Tomato data
+#'
+#' @description
+#' A subset of the time series APU0000712311 from FRED, data on tomato
+#' prices from 2016 to 2026
+#'
+#'
+#' @format ## `lettuce`
+#' A data frame with 119 rows and 5 columns:
+#' \describe{
+#'   \item{date}{a Date object with the data of the data}
+#'   \item{series_id}{APU0000712311, tomato series}
+#'   \item{value}{the average price of tomatoes}
+#'   \item{realtime_start, realtime_end}{The date the data was downloaded}
+#' }
+#' @source <https://fred.stlouisfed.org/series/APU0000712311>
+"tomatoes"
