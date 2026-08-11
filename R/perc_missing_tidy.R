3f0b828261d99b4b957ea99576dbce0f4e66b5ca
Amelia McNamara <amelia.a.mcnamara@gmail.com>
2026-08-11 09:12:24 -0500

    put perc_missing_tidy back to solution from slides

--- R/perc_missing_tidy.R
+++ R/perc_missing_tidy.R
@@ -4,8 +4,8 @@
 #'
 #' A wrapper around [imputeTS::statsNA] that accepts tidy arguments
 #'
-#' @param df a dataset
-#' @param column a variable within that dataset, ideally a time series
+#' @param dataset a dataset
+#' @param variable a variable within that dataset, ideally a time series
 #'
 #' @returns a tibble containing the percent of the variable that was missing
 #' @export
@@ -14,18 +14,18 @@
 #' bacon |> perc_missing_tidy(value)
 #'
 #' lettuce |> perc_missing_tidy(value)
-# perc_missing_tidy <- function(dataset, variable){
-#   var <- substitute(variable)
-#   var_eval <- eval(var, envir = dataset)
-#   na_stats <- imputeTS::statsNA(var_eval, print_only = FALSE)
-#   tibble_perc <- tibble::tibble(perc = na_stats$percentage_NAs)
-#   return(tibble_perc)
-# }
-
-perc_missing_tidy <- function(df, column){
-  x <- df |>
-    dplyr::select({{ column }}) |>
-    dplyr::pull()
-  # sum(is.na(lettuce$value))/length(lettuce$value)
-  statsNA(x, print_only = FALSE)$percentage_NAs
+perc_missing_tidy <- function(dataset, variable){
+  var <- substitute(variable)
+  var_eval <- eval(var, envir = dataset)
+  na_stats <- imputeTS::statsNA(var_eval, print_only = FALSE)
+  tibble_perc <- tibble::tibble(perc = na_stats$percentage_NAs)
+  return(tibble_perc)
 }
+
+# perc_missing_tidy <- function(df, column){
+#   x <- df |>
+#     dplyr::select({{ column }}) |>
+#     dplyr::pull()
+#   # sum(is.na(lettuce$value))/length(lettuce$value)
+#   statsNA(x, print_only = FALSE)$percentage_NAs
+# }
