813ded1f5ed2af3adbbec93010f29865ac437a14
Amelia McNamara <amelia.a.mcnamara@gmail.com>
2026-08-17 15:04:30 -0500

    not quite working with testdat... failing check()

---
+++ tests/testthat/test-data.R
@@ -0,0 +1,8 @@
+set_testdata(bacon)
+
+test_that("bacon data passes", {
+    expect_values(series_id, "APU0000704111")
+    expect_range(value, 0.5, 8)
+    expect_date_formatted(date) # should work
+    expect_equal(dim(bacon), c(119, 5))
+})
