ec7ff24958d9aaf14780f3b251b75ec3c2d2e5a3
Amelia McNamara <amelia.a.mcnamara@gmail.com>
2026-08-17 17:20:04 -0500

    Feels like it should work, but really really does not
    ── Test failures ────────────────────────────────────────────────────────────────── testthat ────
    
    > # This file is part of the standard setup for testthat.
    > # It is recommended that you do not modify it.
    > #
    > # Where should you do additional test configuration?
    > # Learn more about the roles of various files in:
    > # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
    > # * https://testthat.r-lib.org/articles/special-files.html
    >
    > library(testthat)
    Warning message:
    package 'testthat' was built under R version 4.4.3
    > library(BLT)
    >
    > test_check("BLT")
    Error in `source_dir()`:
    ! Failed to evaluate './helper.R'.
    Caused by error:
    ! object 'chk_date_yyyymmdd_formatted' not found
    Backtrace:
         ▆
      1. ├─testthat::test_check("BLT")
      2. │ └─testthat::test_dir(...)
      3. │   └─testthat:::test_files(...)
      4. │     └─testthat:::test_files_serial(...)
      5. │       └─testthat:::test_files_setup_state(...)
      6. │         └─testthat::source_test_helpers(".", env)
      7. │           └─testthat::source_dir(path, "^helper.*\\.[rR]$", env = env, wrap = FALSE)
      8. │             └─base::lapply(...)
      9. │               └─testthat (local) FUN(X[[i]], ...)
     10. │                 └─testthat::source_file(...)
     11. │                   ├─base::withCallingHandlers(...)
     12. │                   └─base::eval(exprs, env)
     13. │                     └─base::eval(exprs, env)
     14. │                       └─testdat::expect_make(chk_date_yyyymmdd_formatted) at ./helper.R:8:1
     15. │                         ├─formals(eval_tidy(enfunc))[-1] %>% as.list
     16. │                         ├─base::formals(eval_tidy(enfunc))
     17. │                         └─rlang::eval_tidy(enfunc)
     18. ├─base::as.list(.)
     19. └─base::.handleSimpleError(...)
     20.   └─testthat (local) h(simpleError(msg, call))
     21.     └─cli::cli_abort(...)
     22.       └─rlang::abort(...)
    Warning messages:
    1: package 'rlang' was built under R version 4.4.3
    2: package 'testdat' was built under R version 4.4.3
    Execution halted
    
    1 error ✖ | 0 warnings ✔ | 2 notes ✖

--- tests/testthat/helper.R
+++ tests/testthat/helper.R
@@ -1,12 +1,10 @@
-# library(rlang)
+library(rlang)
 library(testdat)
-#
-# chk_date_yyyymmdd_formatted <- function(x) {
-#   stringr::str_detect(x, "^[1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]$") & !is.na(lubridate::ymd(x, quiet = TRUE))
-# }
-#
-#
-#
-# expect_date_formatted <- expect_make(chk_date_yyyymmdd_formatted)
+
+chk_date_yyyymmdd_formatted <- function(x) {
+  stringr::str_detect(x, "^[1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9]$") & !is.na(lubridate::ymd(x, quiet = TRUE))
+}
+
+expect_date_formatted <- expect_make(chk_date_yyyymmdd_formatted)
 
 set_testdata(bacon)
--- tests/testthat/test-data.R
+++ tests/testthat/test-data.R
@@ -3,6 +3,6 @@ set_testdata(bacon)
 test_that("bacon data passes", {
     expect_values(series_id, "APU0000704111")
     expect_range(value, 0.5, 8)
-    # expect_date_formatted(date) # should work
+    expect_date_formatted(date) # should work
     expect_equal(dim(bacon), c(119, 5))
 })
